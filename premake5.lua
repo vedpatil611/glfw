40dab9cec27b0f1d11306d6c5f6f91e7b4b97397
vedpatil611 <vedpatil611@gmail.com>
2020-07-12 20:21:30 +0530

    premake script correction

--- premake5.lua
+++ premake5.lua
@@ -6,33 +6,35 @@ project "GLFW"
 	objdir ("intermediate/" .. outputdir .. "/%{prj.name}")
 
 	files {
-		"internal.h",
-		"mappings.h",
-		"context.c", 
-		"init.c",
-		"input.c"
-		"monitor.c"
-		"vulkan.c"
-		"window.c"
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/internal.h",
+		"src/mappings.h",
+		"src/context.c", 
+		"src/init.c",
+		"src/input.c"
+		"src/monitor.c"
+		"src/vulkan.c"
+		"src/window.c"
 	}
 
 	filter "system:windows" {
 		systemversion "latest"
 		files {
-			"win32_platform.h",
-			"win32_joystick.h",
-			"wgl_context.h",
-			"egl_context.h",
-			"osmesa_context.h",
-			"win32_init.c",
-			"win32_joystick.c",
-			"win32_monitor.c",
-			"win32_time.c",
-			"win32_thread.c",
-			"win32_window.c",
-			"wgl_context.c",
-			"egl_context.c",
-			"osmesa_context.c"
+			"src/win32_platform.h",
+			"src/win32_joystick.h",
+			"src/wgl_context.h",
+			"src/egl_context.h",
+			"src/osmesa_context.h",
+			"src/win32_init.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
 		}
 		defines {
 			"_GLFW_WIN32"
