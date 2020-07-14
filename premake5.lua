569aed2a6d68d5223c5f601e3af97434bb4926b0
vedpatil611 <vedpatil611@gmail.com>
2020-07-14 11:18:42 +0530

    Linux support for premake5

--- premake5.lua
+++ premake5.lua
@@ -40,10 +40,33 @@ project "GLFW"
 			"_GLFW_WIN32"
 		}
 
+	filter "system:linux"
+		files {
+			"src/x11_platform.h",
+			"src/xkb_unicode.h",
+			"src/posix_time.h",
+			"src/posix_thread.h",
+			"src/glx_context.h",
+			"src/egl_context.h",
+			"src/osmesa_context.h",
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
+		defines {
+			"_GLFW_X11"
+		}
+
 	filter "configurations:Debug"
 		defines { "DEBUG" }
 		symbols "on"
 
 	filter "configurations:Release"
-		defines { "NDEBUG" }
+		defines { "RELEASE" }
 		optimize "on"
